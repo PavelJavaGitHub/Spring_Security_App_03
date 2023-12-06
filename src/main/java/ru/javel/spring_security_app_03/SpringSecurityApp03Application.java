22d176cb904196fd0201465f3da51aeaa8eb951c
PavelJavaGitHub <mailpashok@gmail.com>
2023-12-06 21:30:27 +0300

    Initial commit

---
+++ src/main/java/ru/javel/spring_security_app_03/SpringSecurityApp03Application.java
@@ -0,0 +1,13 @@
+package ru.javel.spring_security_app_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApp03Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/javel/spring_security_app_03/SpringSecurityApp03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javel.spring_security_app_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApp03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
